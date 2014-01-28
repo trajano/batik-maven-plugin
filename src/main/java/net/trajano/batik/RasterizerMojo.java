3086317378f9effbafdedf572e5cec2c8c20928e
Archimedes Trajano <developer@trajano.net>
2014-01-27 22:05:37 -0500

    Added all the required parameters

--- src/main/java/net/trajano/batik/RasterizerMojo.java
+++ src/main/java/net/trajano/batik/RasterizerMojo.java
@@ -1,9 +1,12 @@
 package net.trajano.batik;
 
+import java.io.File;
+
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
 
 /**
  * Executes the Batik rasterizer.
@@ -11,10 +14,34 @@ import org.apache.maven.plugins.annotations.Mojo;
 @Mojo(name = "rasterizer", defaultPhase = LifecyclePhase.GENERATE_RESOURCES, threadSafe = true, requiresOnline = false)
 public class RasterizerMojo extends AbstractMojo {
     /**
+     * The directory to write the rasterized SVG files.
+     */
+    @Parameter(defaultValue = "${project.build.directory}/generated-resources/batik", required = true)
+    private File destDir;
+
+    /**
+     * The type of file to convert to. Valid values are <code>png</code>,
+     * <code>jpeg</code>, <code>tiff</code> or <code>pdf</code>.
+     */
+    @Parameter(defaultValue = "png", required = true)
+    private String destType;
+
+    /**
+     * The directory containing the SVG files.
+     */
+    @Parameter(defaultValue = "${basedir}/src/main/svg", required = true)
+    private File srcDir;
+    /**
+     * The file pattern for inclusion of SVG files.
+     */
+    @Parameter(defaultValue = "**/*.svg", required = true)
+    private String srcIncludes;
+
+    /**
      * {@inheritDoc}
      */
     @Override
     public void execute() throws MojoExecutionException {
-        getLog().info("Hello, world.");
+        getLog().info(destDir.toString());
     }
 }
