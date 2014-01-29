aac84e0d3b382f38f741f99e8373a26c546e10e7
Archimedes Trajano <developer@trajano.net>
2014-01-29 02:01:48 -0500

    Added suppress warnings

--- src/main/java/net/trajano/batik/internal/SvgFont2.java
+++ src/main/java/net/trajano/batik/internal/SvgFont2.java
@@ -16,16 +16,19 @@ import org.apache.batik.svggen.font.SVGFont;
 public class SvgFont2 extends SVGFont {
     /**
      * Writes the font as an SVG file. This is a convenience method that passes
-     * typical values that are used for the other paramters.
+     * typical values that are used for the other parameters. Warnings are
+     * suppressed to prevent Sonar from complaining about throwing an
+     * {@link Exception}.
      * 
      * @param ps
      *            print stream to write to. (does not get closed)
      * @param font
-     *            font to rend
+     *            font to render
      * @param id
      *            ID of the font
      * @throws Exception
      */
+    @SuppressWarnings("all")
     public static void writeFontAsSvg(final PrintStream ps, final Font font,
             final String id) throws Exception {
         writeFontAsSvg(ps, font, id, -1, -1, true, false);
@@ -33,12 +36,13 @@ public class SvgFont2 extends SVGFont {
 
     /**
      * Writes the font as an SVG file. This is from a segment of code from
-     * {@link SVGFont#main(String[])}.
+     * {@link SVGFont#main(String[])}. Warnings are suppressed to prevent Sonar
+     * from complaining about throwing an {@link Exception}.
      * 
      * @param ps
      *            print stream to write to. (does not get closed)
      * @param font
-     *            font to rend
+     *            font to render
      * @param id
      *            ID of the font
      * @param first
@@ -51,6 +55,7 @@ public class SvgFont2 extends SVGFont {
      *            force ASCII range
      * @throws Exception
      */
+    @SuppressWarnings("all")
     public static void writeFontAsSvg(final PrintStream ps, final Font font,
             @Nullable final String id, final int first, final int last,
             final boolean autoRange, final boolean forceAscii) throws Exception {
